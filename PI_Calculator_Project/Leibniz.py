f500dc31515690251c39bfee28c34daacc6cd1bf
gekabeast <gekabeast01@gmail.com>
2022-09-23 20:05:47 -0500

    updated README and documentated Leibniz program

--- PI_Calculator_Project/Leibniz.py
+++ PI_Calculator_Project/Leibniz.py
@@ -1,12 +1,23 @@
-# calculates pi 
+# calculates pi with the leibniz method. 
 
-pi_part = 1
-denominator = 3
-alternator = 2
+#this program is based off of the leibniz formula for pi. 
+
+#The formula : pi/4 = the infinite alternating series of 1/(2n + 1)
+
+pi_part = 1 #Just the first term in the series. 
+INIT_DENOM = 3 #The first denominator of a "fraction" in the series. 
+ALTERNATOR = 2 #The maginitude of the denominator is changing by this amount
 
 while True:
-    pi_part -= (1/denominator)
-    denominator += alternator
-    pi_part +=(1/denominator)
-    denominator += alternator
-    print(pi_part * 4)
+    
+    #############
+    pi_part -= (1/INIT_DENOM)
+    INIT_DENOM += ALTERNATOR
+    #############
+    #These two sections are for the alternating signs
+    #############
+    pi_part +=(1/INIT_DENOM)
+    INIT_DENOM += ALTERNATOR
+    #############
+    
+    print(pi_part * 4) #pi_part is just pi/4. 
--- README.md
+++ README.md
@@ -1,7 +1,12 @@
 # PersonalProjects
-All of my personal projects (either finished or in-progress)
+
+
+
+Just some of my personal projects over the last few years (either finished or in-progress)
 
 Organized in folders of 
 1. FINISHED 
 or
 2. CURRENTLY_IN_DEVELOPMENT 
+
+
